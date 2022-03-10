e9c57bc72a98db21fdafd74a38555f6aeec4452a
濯茶 <xunxing.mxx@alibaba-inc.com>
2022-03-10 10:30:50 +0800

    update

---
+++ public/zh-cn/nodejs-network-stream-2/read1.js
@@ -0,0 +1,19 @@
+let c = 97
+
+const Stream = require('stream')
+const rs = new Stream.Readable({
+  read() {
+    rs.push(String.fromCharCode(c++))
+    if(c > 'z'.charCodeAt(0)) rs.push(null)
+  }
+});
+
+// or
+// rs._read = function() {
+// }
+
+rs.pipe(process.stdout)
+
+process.on('exit', function () {
+  console.error('\n_read() called ' + (c - 97) + ' times');
+});
