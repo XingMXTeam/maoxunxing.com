fba0c7ee815b47f5a686a46099c19c5a11d3e59d
xunxing <xunxing1989@163.com>
2022-04-27 22:07:35 +0800

    feat: update

--- content/posts/median-of-bst/index.js
+++ content/posts/median-of-bst/index.js
@@ -5,10 +5,38 @@ class TreeNode {
     }
 }
 
+// 中序遍历
 function countNode(root) {
     let current, pre
     let count = 0
+    if(root == null)
+        return count
     
+    current = root
+    while(current != null) {
+        if(current.left === null) {
+            count++
+            current = current.right
+        }
+        else {
+            // 找到前任节点
+            pre = current.left
+            while(pre.right !== null && pre.right !== current) {
+                pre = pre.right
+            }
+            
+            if(pre.right === null) {
+                pre.right = current
+                current = current.left
+            }
+            else {
+                pre.right = null
+                count++
+                current = current.right
+            }
+        }
+    }
+    return count
 }
 
 function getMedian(node) {
